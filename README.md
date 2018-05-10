78a86ec040e12d95692adfdabbe10f06f648571a
John F <johnnyvf24@gmail.com>
2018-05-09 21:14:44 -0600

    Zeromq events via pub-sub

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# hellochess
+# Example
 
 Pre-reqs:
 1. Install Zeromq
-2. Run the publisher (publishes updates every 5 seconds)
+2. Install Node > 6
+3. `npm i`
+4. Run the publisher (publishes updates through zmq every 5 seconds)
+`npm run publisher`
 
-1. `npm i`
-2. `npm start`
+To proxy updates via node.js, in a seperate terminal (subscribed to events):
+1. `npm start`
 
 For dev:
 1. `npm i -g nodemon`
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+var zmq = require('zmq');
+var subscriber = zmq.socket('sub');
 const port = process.env.port || 3000;
 
 const http = require('http').createServer(app);
@@ -12,11 +14,18 @@ http.listen(port, () => {
   console.log(`express server listening on port ${port}`);
 });
 
+subscriber.connect('tcp://localhost:5563');
+subscriber.subscribe('B');
+let count = 0;
 io.on('connection', (socket) => {
   console.log(`A new client connected with id: ${socket.id}`);
-  setInterval(() => {
-    socket.emit('notification', {info: true, done: 'yup'});
-  }, 1000 * 5); //update every 5 seconds
+
+  subscriber.on('message', function() {
+    var msg = [];
+      Array.prototype.slice.call(arguments).forEach(function(arg) {
+          socket.emit('notification', {info: arg.toString(), count: count++});
+      });
+  });
 });
 
 process.on("uncaughtException", function(err) {
